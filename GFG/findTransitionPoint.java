914aedf49f2d2c0e71e62116ba6b489b6d730a03
Ayush Shukla <123shuklaayush@gmail.com>
2022-09-19 23:41:31 +0530

    Initial Commit

--- GFG/findTransitionPoint.java
+++ GFG/findTransitionPoint.java
@@ -1,21 +1,14 @@
 package GFG;
 
 public class findTransitionPoint {
-    int transitionPoint(int arr[], int n) {
-        // code here
-        int k = arr[0];
-        if(arr.length ==1){
-            return 1;
-        }
-        for(int i= 0;i<n; i++){
-            if(k!=arr[i]){
+    static int transitionPoint(int arr[], int n) {
+        for(int i=0; i<n ; i++){
+            if(arr[i]==1){
                 return i;
-            }
-        }
+            }}
         return -1;
     }
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int[] arr = {0,0,1,1};
         System.out.println(transitionPoint(arr, arr.length));
     }
